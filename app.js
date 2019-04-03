b2ab47ff414acafb1dbb3929ef88796bab45359f
Farah Student <farah.gus@hotmail.com>
2019-04-03 10:55:08 -0400

    app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+
+
--- server.js
+++ server.js
@@ -1,19 +1,4 @@
-const express = require("express");
-const morgan = require("morgan");
-
-const app = express();
-
-app.use(morgan("dev"));
-
-
-
-
-
-
-
-
-
-
+const app = require('./app');
 
 const PORT = 8000;
 app.listen(PORT, () => {
