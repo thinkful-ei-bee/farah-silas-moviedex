81cbb473d5fba661cea3d182ee697b72a11c663c
Silas <silasishallahan@gmail.com>
2019-04-03 11:14:41 -0700

    installed middleware

--- app.js
+++ app.js
@@ -1,44 +1,53 @@
-const express = require("express");
-require('dotenv').config()
-const morgan = require("morgan");
-const MOVIEDEX = require('./movie.json')
+'use strict';
 
+// Variables and libraries
+require('dotenv').config();
+const cors = require('cors');
+const helmet = require('helmet');
+const express = require('express');
+const morgan = require('morgan');
+const MOVIEDEX = require('./movie.json');
 const app = express();
 
-app.use(morgan("dev"));
+// Middleware
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
 
 app.use(function validateBearerToken(req, res, next) {
-    // api token 
-    const apiToken = process.env.API_TOKEN;
-    const authToken = req.get("Authorization");
+  // api token 
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
 
-    if (!authToken || authToken.split(" ")[1] !== apiToken) {
-        return res.status(401).json({error: "Unauthorized request"});
-      }
-      // move to the next middleware
-      next();
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({error: 'Unauthorized request'});
+  }
+  // move to the next middleware
+  next();
 });
 
+
+// Endpoints
 app.get('/movie', (req, res) => {
-    const {genreQuery, countryQuery, avg_voteQuery} = req.query;
-    let results = MOVIEDEX
+  const {genreQuery, countryQuery, avg_voteQuery} = req.query;
+  let results = MOVIEDEX;
 
-    if(genreQuery){
-        results = results.filter(movieGenre => 
-           movieGenre.genre.toLowerCase().includes(genreQuery.toLowerCase()));
-    }
+  if(genreQuery){
+    results = results.filter(movieGenre => 
+      movieGenre.genre.toLowerCase().includes(genreQuery.toLowerCase()));
+  }
 
-    if(countryQuery){
-        results = results.filter(movieCountry => 
-            movieCountry.country.toLowerCase().includes(countryQuery.toLowerCase()));
-    }
-    if(avg_voteQuery){
-        results = results.filter(movieVote => 
-            movieVote.avg_vote >= avg_voteQuery
-            )}
+  if(countryQuery){
+    results = results.filter(movieCountry => 
+      movieCountry.country.toLowerCase().includes(countryQuery.toLowerCase()));
+  }
+  if(avg_voteQuery){
+    results = results.filter(movieVote => 
+      movieVote.avg_vote >= avg_voteQuery
+    );}
 
 
-    res.json(results);
+  res.json(results);
 });
 
 
