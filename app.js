fccc8236d140807441fd31ebaf6a0fc1968fe907
Farah Student <farah.gus@hotmail.com>
2019-04-03 11:47:35 -0400

    search queries done

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 const express = require("express");
 const morgan = require("morgan");
+const MOVIEDEX = require('./movie.json')
 
 const app = express();
 
 app.use(morgan("dev"));
 
 
+// app.use(function validateBearerToken(req, res, next) {});
+//     // api token 
 
 
+app.get('/movie', (req, res) => {
+    const {genreQuery, countryQuery, avg_voteQuery} = req.query;
+    let results = MOVIEDEX
+
+    if(genreQuery){
+        results = results.filter(movieGenre => 
+           movieGenre.genre.toLowerCase().includes(genreQuery.toLowerCase()));
+    }
+
+    if(countryQuery){
+        results = results.filter(movieCountry => 
+            movieCountry.country.toLowerCase().includes(countryQuery.toLowerCase()));
+    }
+    if(avg_voteQuery){
+        results = results.filter(movieVote => 
+            movieVote.avg_vote >= avg_voteQuery
+            )}
 
 
-
-
+    res.json(results);
+});
 
 
 module.exports = app;
+
+
 
 
